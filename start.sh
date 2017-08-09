31b513fc962fdf3528c38387f77cad815a38c284
Multipixelone <finn@cnwr.net>
2017-08-08 19:06:38 -0700

    Updated start.sh

--- start.sh
+++ start.sh
@@ -2,5 +2,6 @@
 
 # Start Flash Server (Debug for Now...)
 
+echo "KittyTalk by Multipixelone!"
 export FLASK_APP=KittyTalk.py
-python -m flask run --host=0.0.0.0
+python -m flask run --host=0.0.0.0 --port=80
